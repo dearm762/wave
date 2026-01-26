13acc250bfbb45bc927250cae998885692167256
Abdurrauf <misterfighter1990@gmail.com>
2026-01-26 23:02:37 +0500

    switch to swc compiler

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-import { PrismaClient } from 'generated/prisma/client'
+import { PrismaClient } from 'src/generated/prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient {
