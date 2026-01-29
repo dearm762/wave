04134cbdb0275a50103d888bddde8663f4c17816
Abdurrauf <misterfighter1990@gmail.com>
2026-01-29 10:05:59 +0500

    created email service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { EmailService } from './email/email.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule],
-  providers: [PrismaService],
+  providers: [PrismaService, EmailService],
 })
 export class AppModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -36,5 +36,4 @@ export class AuthService {
 		email: string,
 		message: string
 	) { }
-
 }
